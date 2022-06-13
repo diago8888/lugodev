5f8b4f095db722b6ddae0aaf11477bd3abd6ed67
diago8888 <102332780+diago8888@users.noreply.github.com>
2022-06-12 21:58:07 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ def process_message(update, context):
 
 if __name__ == '__main__':
 
-    updater = Updater(token=os.environ['5327523213:AAFwwijRyEXsTKChHHZemT_jfh-ugLHCy-c'], use_context=True)
+    updater = Updater(token=('5327523213:AAFwwijRyEXsTKChHHZemT_jfh-ugLHCy-c'), use_context=True)
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(filters=Filters.text, callback=process_message))
     updater.start_polling()
